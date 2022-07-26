c7359dd16c24c844dc565444cac3c89934475b98
Nwdle <83025574+vaalfer@users.noreply.github.com>
2022-07-26 10:26:53 -0400

    Update clave.js

--- numero 3/assets/js/clave.js
+++ numero 3/assets/js/clave.js
@@ -4,8 +4,9 @@ function password () {
     value = document.querySelector("#input").value
     pass = document.querySelector("#clave")
     if (value == 911) {
-        pass.textContent = "Lo lograste UwU"
-    }else{
-        pass.textContent = "Clave inválida unu"
-    }
-}
+        pass.textContent = "Lo lograste UwU" }
+    else if (value == 744) {
+        pass.textContent = "Lo lograste UwU" }
+    else{
+        pass.textContent = "Clave inválida unu"}
+
